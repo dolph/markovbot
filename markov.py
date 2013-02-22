ff749dd3d7f5a6865a9e17148e62e43b2ee6524a
Dolph Mathews <dolph.mathews@gmail.com>
2013-02-21 20:32:10 -0600

    Upgraded to longer markov chains

--- markov.py
+++ markov.py
@@ -15,8 +15,9 @@ class Link(Base):
     __tablename__ = 'links'
 
     id = sql.Column(sql.Integer, primary_key=True)
-    word = sql.Column(sql.String, index=True)
-    next_word = sql.Column(sql.String)
+    word1 = sql.Column(sql.String, index=True)
+    word2 = sql.Column(sql.String, index=True)
+    word3 = sql.Column(sql.String)
 
 
 Base.metadata.create_all(engine)
@@ -28,16 +29,33 @@ def reset():
     session.commit()
 
 
-def add_link(w1, w2):
+def add_link(w1, w2, w3):
     session = Session()
-    ref = Link(word=w1, next_word=w2)
+    ref = Link(word1=w1, word2=w2, word3=w3)
     session.add(ref)
     session.commit()
 
 
-def get_next(w=None):
+def list_first():
     session = Session()
-    words = [r.next_word for r in session.query(Link).filter_by(word=w).all()]
+    refs = session.query(Link).filter_by(word1=None).all()
+    return [(ref.word2, ref.word3) for ref in refs]
+
+
+def get_first(*args):
+    words = list_first(*args)
+    random.shuffle(words)
+    return words.pop()
+
+
+def list_next(w1, w2):
+    session = Session()
+    refs = session.query(Link).filter_by(word1=w1, word2=w2).all()
+    return [ref.word3 for ref in refs]
+
+
+def get_next(*args):
+    words = list_next(*args)
     random.shuffle(words)
     return words.pop()
 
@@ -61,23 +79,32 @@ def consume(s):
     s = s.split('-')
 
     for i, w in enumerate(s):
+        w1 = s[i]
+        w2 = s[i + 1] if i + 1 < len(s) else None
+        w3 = s[i + 2] if i + 2 < len(s) else None
+
         if i == 0:
-            add_link(None, w)
+            add_link(None, w1, w2)
 
-        if i + 1 == len(s):
-            w2 = None
-        else:
-            w2 = s[i + 1]
-
-        add_link(w, w2)
+        if w2 is not None:
+            add_link(w1, w2, w3)
 
 
 def produce(w=None):
     s = []
 
-    w = get_next()
-    while w:
-        s.append(w)
-        w = get_next(w)
+    w1, w2 = get_first()
+    s.append(w1)
+    s.append(w2)
+
+    w3 = True
+    while w3:
+        w3 = get_next(w1, w2)
+
+        if w3 is not None:
+            s.append(w3)
+
+        w1 = w2
+        w2 = w3
 
     return ' '.join(s)
--- tests.py
+++ tests.py
@@ -9,54 +9,34 @@ class TestChains(unittest.TestCase):
 
     def test_consume_str(self):
         markov.consume('This is a very simple test.')
-        """
-        self.assertEqual(markov._DATA, {
-            None: ['this'],
-            'this': ['is'],
-            'is': ['a'],
-            'a': ['very'],
-            'very': ['simple'],
-            'simple': ['test'],
-            'test': [None],
-        })
-        """
+
+        self.assertEqual(markov.list_next(None, 'this'), ['is'])
+        self.assertEqual(markov.list_next('this', 'is'), ['a'])
+        self.assertEqual(markov.list_next('is', 'a'), ['very'])
+        self.assertEqual(markov.list_next('a', 'very'), ['simple'])
+        self.assertEqual(markov.list_next('very', 'simple'), ['test'])
+        self.assertEqual(markov.list_next('simple', 'test'), [None])
 
     def test_consume_str_with_multiple_paths(self):
         markov.consume('This is a test and is a loop.')
-        """
-        self.assertEqual(markov._DATA, {
-            None: ['this'],
-            'this': ['is'],
-            'is': ['a', 'a'],
-            'a': ['test', 'loop'],
-            'test': ['and'],
-            'and': ['is'],
-            'loop': [None],
-        })
-        """
+
+        self.assertEqual(markov.list_next(None, 'this'), ['is'])
+        self.assertEqual(markov.list_next('this', 'is'), ['a'])
+        self.assertEqual(markov.list_next('is', 'a'), ['test', 'loop'])
+        self.assertEqual(markov.list_next('a', 'test'), ['and'])
+        self.assertEqual(markov.list_next('test', 'and'), ['is'])
+        self.assertEqual(markov.list_next('and', 'is'), ['a'])
+        self.assertEqual(markov.list_next('a', 'loop'), [None])
 
     def test_consume_multiple_strings(self):
         markov.consume('This is a test.')
         markov.consume('This is a statement.')
         markov.consume('This sentence is short.')
-        """
-        self.assertEqual(markov._DATA, {
-            None: ['this', 'this', 'this'],
-            'this': ['is', 'is', 'sentence'],
-            'sentence': ['is'],
-            'is': ['a', 'a', 'short'],
-            'a': ['test', 'statement'],
-            'statement': [None],
-            'short': [None],
-            'test': [None],
-        })
-        """
 
         self.assertIn(markov.produce(), [
             'this is a test',
             'this is a statement',
             'this sentence is short',
-            'this is short',
             'this sentence is a test',
             'this sentence is a statement',
         ])
