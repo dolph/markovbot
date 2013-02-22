6a5b6bb13b5fbd803b24a13a012c6b5d0de555b1
Dolph Mathews <dolph.mathews@gmail.com>
2013-02-22 16:15:26 -0600

    Ignoring lots of unknown words

--- markov.py
+++ markov.py
@@ -3,6 +3,47 @@ import re
 import db
 
 
+_DICTIONARY = None
+_IGNORED_WORDS = None
+_ALL_WORDS = None
+
+def load_words(filename):
+    try:
+        fp = open(filename, 'r')
+        words = fp.readlines()
+    finally:
+        fp.close()
+
+    return set(word.strip() for word in words)
+
+
+def get_dictionary():
+    global _DICTIONARY
+
+    if _DICTIONARY is None:
+        _DICTIONARY = load_words('popular.txt')
+
+    return _DICTIONARY
+
+
+def get_ignored_words():
+    global _IGNORED_WORDS
+
+    if _IGNORED_WORDS is None:
+        _IGNORED_WORDS = load_words('ignore.txt')
+
+    return _IGNORED_WORDS
+
+
+def get_all_words():
+    global _ALL_WORDS
+
+    if _ALL_WORDS is None:
+        _ALL_WORDS = get_dictionary().union(get_ignored_words())
+
+    return _ALL_WORDS
+
+
 def slugify(s):
     s = s.lower()
     s = s.replace(' ', '_')
@@ -17,7 +58,38 @@ def slugify(s):
     return s
 
 
+def simplify(s):
+    s = s.split(' ')
+
+    for i, w in enumerate(s):
+        slug = slugify(w)
+
+        if slug in get_dictionary():
+            s[i] = slug
+
+    return ' '.join(s)
+
+
+def topics(s):
+    topics = set(slugify(s).split('-'))
+    topics = topics.difference(get_ignored_words())
+    return topics
+
+
+def useful(s):
+    s = set(slugify(s).split('-'))
+    pronouns = s.difference(get_all_words())
+    pronoun_ratio = 1.0 * len(pronouns) / len(s)
+    return pronoun_ratio <= .15
+
+
 def consume(s):
+    s = simplify(s)
+
+    if not s or not useful(s):
+        print 'Ignoring', s
+        return
+
     s = s.split(' ')
 
     for i, w in enumerate(s):
--- tests.py
+++ tests.py
@@ -11,28 +11,28 @@ class TestChains(unittest.TestCase):
     def test_consume_one_word(self):
         markov.consume('Word.')
 
-        self.assertEqual(db.list_next(None, 'Word.'), [None])
+        self.assertEqual(db.list_next(None, 'word'), [None])
 
     def test_consume_str(self):
         markov.consume('This is a very simple test.')
 
-        self.assertEqual(db.list_next(None, 'This'), ['is'])
-        self.assertEqual(db.list_next('This', 'is'), ['a'])
+        self.assertEqual(db.list_next(None, 'this'), ['is'])
+        self.assertEqual(db.list_next('this', 'is'), ['a'])
         self.assertEqual(db.list_next('is', 'a'), ['very'])
         self.assertEqual(db.list_next('a', 'very'), ['simple'])
-        self.assertEqual(db.list_next('very', 'simple'), ['test.'])
-        self.assertEqual(db.list_next('simple', 'test.'), [None])
+        self.assertEqual(db.list_next('very', 'simple'), ['test'])
+        self.assertEqual(db.list_next('simple', 'test'), [None])
 
     def test_consume_str_with_multiple_paths(self):
         markov.consume('This is a test and is a loop.')
 
-        self.assertEqual(db.list_next(None, 'This'), ['is'])
-        self.assertEqual(db.list_next('This', 'is'), ['a'])
-        self.assertEqual(db.list_next('is', 'a'), ['test', 'loop.'])
+        self.assertEqual(db.list_next(None, 'this'), ['is'])
+        self.assertEqual(db.list_next('this', 'is'), ['a'])
+        self.assertEqual(db.list_next('is', 'a'), ['test', 'loop'])
         self.assertEqual(db.list_next('a', 'test'), ['and'])
         self.assertEqual(db.list_next('test', 'and'), ['is'])
         self.assertEqual(db.list_next('and', 'is'), ['a'])
-        self.assertEqual(db.list_next('a', 'loop.'), [None])
+        self.assertEqual(db.list_next('a', 'loop'), [None])
 
     def test_consume_multiple_strings(self):
         markov.consume('This is a test.')
@@ -40,16 +40,38 @@ class TestChains(unittest.TestCase):
         markov.consume('This sentence is short.')
 
         self.assertIn(markov.produce(), [
-            'This is a test.',
-            'This is a statement.',
-            'This sentence is short.',
-            'This sentence is a test.',
-            'This sentence is a statement.',
+            'this is a test',
+            'this is a statement',
+            'this sentence is short',
+            'this sentence is a test',
+            'this sentence is a statement',
         ])
 
     def test_produce_string(self):
         markov.consume('This is a very simple test.')
-        self.assertEqual(markov.produce(), 'This is a very simple test.')
+        self.assertEqual(markov.produce(), 'this is a very simple test')
+
+
+class TestReduction(unittest.TestCase):
+    def test_topics(self):
+        s = markov.topics('Hello, my name is Dolph, how are you?')
+        self.assertEqual(s, set(['hello', 'name', 'dolph']))
+
+    def test_topics_archer(self):
+        s = markov.topics(
+            'Pam: So are all kinds of shit, but look at these odds. Half the '
+            'people that work here are field agents...')
+        self.assertEqual(
+            s,
+            set(['pam', 'shit', 'odds', 'people', 'field', 'work', 'agents']))
+
+    def test_simplify(self):
+        s = markov.simplify('Hello, my name is Dolph, how are you?')
+        self.assertEqual(s, 'hello my name is Dolph, how are you')
+
+    def test_useful(self):
+        self.assertTrue(markov.useful('Hello, my name is Dolph, how are you?'))
+        self.assertFalse(markov.useful('This is a link to Google http://google.com/'))
 
 
 if __name__ == '__main__':
